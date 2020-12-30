fc03a172ea23cb62da02bda6effcc1a17a080e7f
Ameer <Ameer@DESKTOP-3V8DTOO>
2020-12-30 10:50:25 +0500

    First commit - Solution

--- src/UI/main.java
+++ src/UI/main.java
@@ -4,6 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int x=5,y=10;
+		
+		int z=x+y;
+		
+		System.out.println("Value of after addition : " +z);
+		
 
 	}
 
--- src/sanity/main.java
+++ src/sanity/main.java
@@ -4,6 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+int x=5;
+		
+		int z=x*x;
+		
+		System.out.println("Value of after square: " +z);
+		
 
 	}
 
